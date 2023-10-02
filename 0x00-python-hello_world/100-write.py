e37d1247e2c738d757b84647b078d1b98a1afe6a
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-10-02 12:25:45 -0700

    Write.

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+# Write the message to the stderr.
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Your code should exit with status code 1
+sys.exit(1)
