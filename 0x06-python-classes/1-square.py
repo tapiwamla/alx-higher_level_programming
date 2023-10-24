d38f87d6b1c9862891e27909d00dcc18bc2f9f69
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-10-24 10:07:48 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module Sqaure """
+
+
+class Square:
+    """ The square class defined by the geometric shape. """
+    def __init__(self, size):
+        """
+        Initialize a method.
+        """
+        self.__size = size
