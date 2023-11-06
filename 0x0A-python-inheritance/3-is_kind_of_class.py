0c901aff1ca70439416e317dd4f3d5a3efd80270
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-11-06 09:42:22 -0800

    Is kind of class.

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+is_kind_of_class module.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Return: True if the object is an instance of
+    or if the object is an instance of a class that inherited from,
+    the specified class.
+    False otherwise.
+    """
+    return isinstance(obj, a_class)
