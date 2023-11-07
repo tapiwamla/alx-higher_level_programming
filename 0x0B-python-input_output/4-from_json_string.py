d3e0eb960e5ed43904af57ca1b0ed4f17d0b67ac
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-11-07 10:33:07 -0800

    From JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Returns an object (Python DS) represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python DS) represented by a JSON string.
+    """
+    return json.loads(my_str)
